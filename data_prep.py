a294d6a1e213934ab6a368bea9809500943f6a61
Jeremy Dalay <jeremydalay@gmail.com>
2021-10-25 03:08:27 +0800

    Update

--- data_prep.py
+++ data_prep.py
@@ -1,1 +1,8 @@
-#TODO: CRYPTIC Data Preparation 
+import numpy as np
+
+def init_lstm():
+    value_to_idx = 'dict value:idx'
+    idx_to_value = 'dict idx:value'
+    seq_size = 'Length of unique values'
+
+    return value_to_idx,idx_to_value,len(seq_size)
